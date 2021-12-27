1bb08cd4975424da582dd4a65f916ed3de3ff917
Rocco Petruccio <petruccio.rocco@gmail.com>
2021-12-27 19:21:39 +0000

    Simplification and Bug Fixes

--- AITic.java
+++ AITic.java
@@ -2,81 +2,66 @@
 class AITic {
   String piece;
 
-  // To block the wins via the middle
-  boolean middleTop = true;
-  boolean middleLeft  = true;
-  boolean middleRight = true;
-  boolean middleBottom = true;
-
-  boolean topRightAndMiddle = true;
-  boolean topLeftAndMiddle = true;
-  boolean bottomRightAndMiddle = true;
-  boolean bottomLeftAndMiddle = true;
-
-  // To block the corners
-  boolean topRightCorner = true;
-  boolean topLeftCorner = true;
-  boolean bottomRightCorner = true;
-  boolean bottomLeftCorner = true;
-
   public String checker(String[][] board, String playerPiece) {
     if (board[2][2].equals("|_|")) {
       board[2][2] = "|" + piece + "|";
       return "";
+    } // The else if statements below are used to block the player from winning by the middles.
+    else if (board[2][2].equals("|" + playerPiece + "|") && board[3][2].equals("|" + playerPiece + "|") && board[1][2] == "|_|") {
+      board[1][2] = "|" + piece + "|";
+      return "";
+    } else if (board[2][2].equals("|" + playerPiece + "|") && board[2][1].equals("|" + playerPiece + "|") && board[2][3] == "|_|") {
+      board[2][3] = "|" + piece + "|";
+      return "";
+    } else if (board[2][2].equals("|" + playerPiece + "|") && board[2][3].equals("|" + playerPiece + "|") && board[2][1] == "|_|") {
+      board[2][1] = "|" + piece + "|";
+      return "";
+    } else if (board[2][2].equals("|" + playerPiece + "|") && board[1][2].equals("|" + playerPiece + "|") && board[3][2] == "|_|") {
+      board[3][2] = "|" + piece + "|";
+      System.out.println("test");
+      return "";
     } // The if else statements below are used to check if the player is going to win by getting three in a row.
          // Note that the win is only being considered for the sides, not the middle square.
-    else if (board[1][1].equals("|" + playerPiece + "|") && board[3][1].equals("|" + playerPiece + "|") && middleLeft == true) {
+    else if (board[1][1].equals("|" + playerPiece + "|") && board[3][1].equals("|" + playerPiece + "|") && board[2][1] == "|_|") {
       board[2][1] = "|" + piece + "|";
-      this.middleLeft = false;
       return "";
-    } else if (board[1][1].equals("|" + playerPiece + "|") && board[1][3].equals("|" + playerPiece + "|") && middleTop == true) {
+    } else if (board[1][1].equals("|" + playerPiece + "|") && board[1][3].equals("|" + playerPiece + "|") && board[1][2] == "|_|") {
       board[1][2] = "|" + piece + "|";
-      this.middleTop = false;
       return "";
-    } else if (board[1][3].equals("|" + playerPiece + "|") && board[3][3].equals("|" + playerPiece + "|") && middleRight == true) {
+    } else if (board[1][3].equals("|" + playerPiece + "|") && board[3][3].equals("|" + playerPiece + "|") && board[2][3] == "|_|") {
       board[2][3] = "|" + piece + "|";
-      this.middleRight = false;
       return "";
-    } else if (board[3][1].equals("|" + playerPiece + "|") && board[3][3].equals("|" + playerPiece + "|") && middleBottom == true) {
+    } else if (board[3][1].equals("|" + playerPiece + "|") && board[3][3].equals("|" + playerPiece + "|") && board[3][2] == "|_|") {
       board[3][2] = "|" + piece + "|";
-      this.middleBottom = false;
       return "";
     } // The else if statement below is used in case the middle spot is taken.
     else if (!(board[2][2].equals("|_|")))
-      if (board[2][2].equals("|" + playerPiece + "|") && board[1][3].equals("|" + playerPiece + "|") && topRightAndMiddle == true) {
-        this.topRightAndMiddle = false;
+      if (board[2][2].equals("|" + playerPiece + "|") && board[1][3].equals("|" + playerPiece + "|") && board[3][1] == "|_|") {
         board[3][1] = "|" + piece + "|";
         return "";
-      } else if (board[2][2].equals("|" + playerPiece + "|") && board[1][1].equals("|" + playerPiece + "|") && topLeftAndMiddle == true) {
-        this.topLeftAndMiddle = false;
+      } else if (board[2][2].equals("|" + playerPiece + "|") && board[1][1].equals("|" + playerPiece + "|") && board[3][3] == "|_|") {
         board[3][3] = "|" + piece + "|";
         return "";
-      } else if (board[2][2].equals("|" + playerPiece + "|") && board[3][1].equals("|" + playerPiece + "|") && bottomLeftAndMiddle == true) {
-        this.bottomLeftAndMiddle = false;
+      } else if (board[2][2].equals("|" + playerPiece + "|") && board[3][1].equals("|" + playerPiece + "|") && board[1][3] == "|_|") {
         board[1][3] = "|" + piece + "|";
         return "";
-      } else if (board[2][2].equals("|" + playerPiece + "|") && board[3][3].equals("|" + playerPiece + "|") && bottomRightAndMiddle == true) {
-        this.bottomRightAndMiddle = false;
+      } else if (board[2][2].equals("|" + playerPiece + "|") && board[3][3].equals("|" + playerPiece + "|") && board[1][1] == "|_|") {
         board[1][1] = "|" + piece + "|";
         return "";
       } else {
         for (int i = 1; i < 4; i += 2) {
           for (int x = 1; x < 4; x += 2) {
-            if (board[1][1].equals("|" + playerPiece + "|") && (this.bottomLeftCorner == true) && !(board[3][3].equals("|" + playerPiece + "|"))) {
+            if (board[1][1].equals("|" + playerPiece + "|") && board[3][3] == "|_|") {
               board[3][3] = "|" + piece + "|";
-              this.bottomLeftCorner = false;
               return "";
-            } else if (board[3][3].equals("|" + playerPiece + "|") && (this.topLeftCorner == true) && !(board[1][1].equals("|" + playerPiece + "|"))) {
+            } else if (board[3][3].equals("|" + playerPiece + "|") && board[1][1] == "|_|") {
               board[1][1] = "|" + piece + "|";
-              this.topLeftCorner = false;
               return "";
-            } else if (board[1][3].equals("|" + playerPiece + "|") && (this.topRightCorner == true) && !(board[3][1].equals("|" + playerPiece + "|"))) {
+            } else if (board[1][3].equals("|" + playerPiece + "|") && board[3][1] == "|_|") {
               board[3][1] = "|" + piece + "|";
-              this.topRightCorner = false;
               return "";
-            } else if (board[3][1].equals("|" + playerPiece + "|") && (this.bottomRightCorner == true) && !(board[1][3].equals("|" + playerPiece + "|"))) {
+            } else if (board[3][1].equals("|" + playerPiece + "|") && board[1][3] == "|_|") {
               board[1][3] = "|" + piece + "|";
-              this.bottomRightCorner = false;
               return "";
             } else if (board[i][x].equals("|_|")) {
               board[i][x] = "|" + piece + "|";
@@ -85,6 +70,7 @@ class AITic {
           }
         }
       }
+      System.out.println("test");
       return "";
     }
   }
